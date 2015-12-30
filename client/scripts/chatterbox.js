f63b86c867fb77d607148e7c0302746e2a0bdcf7
ksiddana <ksiddana>
2015-12-29 17:04:55 -0800

    Got the Application to respond to the Server

--- client/scripts/chatterbox.js
+++ client/scripts/chatterbox.js
@@ -8,7 +8,8 @@ $(function() {
   app = {
 //TODO: The current 'addFriend' function just adds the class 'friend'
 //to all messages sent by the user
-    server: 'http://127.0.0.1:3000/',
+    // server: 'http://127.0.0.1:3000/classes/chatterbox',
+    server: 'http://127.0.0.1:3000',
     username: 'anonymous',
     roomname: 'lobby',
     lastMessageId: 0,
@@ -31,14 +32,14 @@ $(function() {
       app.$roomSelect.on('change', app.saveRoom);
 
       // Fetch previous messages
-      app.startSpinner();
+      // app.startSpinner();
       app.fetch(false);
 
       // Poll for new messages
-      // setInterval(app.fetch, 3000);
+      setInterval(app.fetch, 3000);
     },
     send: function(data) {
-      app.startSpinner();
+      // app.startSpinner();
       // Clear messages input
       app.$message.val('');
 
@@ -63,7 +64,7 @@ $(function() {
         url: app.server,
         type: 'GET',
         contentType: 'application/json',
-        data: {},
+        data: {order: "-createdAt"},
         success: function(data) {
           console.log('chatterbox: Messages fetched');
 
@@ -73,7 +74,7 @@ $(function() {
           // Get the last message
           var mostRecentMessage = data.results[data.results.length-1];
           var displayedRoom = $('.chat span').first().data('roomname');
-          app.stopSpinner();
+          // app.stopSpinner();
           // Only bother updating the DOM if we have a new message
           if (mostRecentMessage.objectId !== app.lastMessageId || app.roomname !== displayedRoom) {
             // Update the UI with the fetched rooms
@@ -98,7 +99,7 @@ $(function() {
       // Clear existing messages
 
       app.clearMessages();
-      app.stopSpinner();
+      // app.stopSpinner();
       if (Array.isArray(results)) {
         // Add all fetched messages
         results.forEach(app.addMessage);
@@ -164,7 +165,7 @@ $(function() {
         $message.text(data.text).appendTo($chat);
 
         // Add the message to the UI
-        app.$chats.append($chat);
+        app.$chats.prepend($chat);
       }
     },
     addFriend: function(evt) {
@@ -203,7 +204,7 @@ $(function() {
         }
       }
       else {
-        app.startSpinner();
+        // app.startSpinner();
         // Store as undefined for empty names
         app.roomname = app.$roomSelect.val();
 
@@ -222,15 +223,15 @@ $(function() {
 
       // Stop the form from submitting
       evt.preventDefault();
-    },
-    startSpinner: function(){
-      $('.spinner img').show();
-      $('form input[type=submit]').attr('disabled', "true");
-    },
-
-    stopSpinner: function(){
-      $('.spinner img').fadeOut('fast');
-      $('form input[type=submit]').attr('disabled', null);
     }
+    // startSpinner: function(){
+    //   $('.spinner img').show();
+    //   $('form input[type=submit]').attr('disabled', "true");
+    // },
+
+    // stopSpinner: function(){
+    //   $('.spinner img').fadeOut('fast');
+    //   $('form input[type=submit]').attr('disabled', null);
+    // }
   };
 }());
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,66 +1,34 @@
 var fs = require('fs');
-var path = require('path');
-/*************************************************************
+var path = require('path');    
 
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-  var dataObj = {
+var dataObj = {
     results: [],
   };
  
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
 
 exports.requestHandler = function(request, response) {
   var content = '';
   var fileName = path.basename(request.url);
   var parentFolder = process.cwd()
   var localFolder = parentFolder + '/client';
-
-
-
-
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-
-  // The outgoing status.
   var statusCode = 200;
-
-  // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
 
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/html";
+  headers['Content-Type'] = "application/json";
     console.log(request.url)
   
-  // if(request.url !== "/classes/room1" && request.url !== "/classes/messages" && request.url !== "/classes/room") {
-  //   console.log(request.url)
-  //   statusCode = 404;
-  //   response.writeHead(statusCode, headers);
-  //   response.end();
-  // } else 
+/*  if(request.url !== "/classes/room1" && request.url !== "/classes/messages" && request.url !== "/classes/room") {
+    console.log(request.url)
+    statusCode = 404;
+    response.writeHead(statusCode, headers);
+    response.end();
+  } else */
 
   if ( request.method === 'GET' ) {
 
@@ -70,24 +38,15 @@ exports.requestHandler = function(request, response) {
     
     response.writeHead(statusCode, headers);
     
-    fs.readFile(content, function(err, contents){
-      if(!err){
-      console.log("this is the localFolder name******* ", contents);
-        response.end(contents)
-      } else {
-        console.dir(err);
-      } 
-    });
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-    // response.end(JSON.stringify(dataObj));
+    // fs.readFile(content, function(err, contents){
+    //   if(!err){
+    //   console.log("this is the localFolder name******* ", contents);
+    //     response.end(contents)
+    //   } else {
+    //     console.dir(err);
+    //   } 
+      response.end(JSON.stringify(dataObj));
+ 
   } else if ( request.method === 'POST' ) {
     console.log("Serving request type " + request.method + " for url " + request.url);
     statusCode = 201;
@@ -95,32 +54,22 @@ exports.requestHandler = function(request, response) {
     request.on('data', function(chunk){
       console.log("Received body data:");
       // console.log(chunk.toString())
-      // dataObj.results.push(JSON.parse(chunk))
+      dataObj.results.push(JSON.parse(chunk))
       console.log(dataObj.results)
 
     });
 
     request.on('end', function (){
       response.writeHead(statusCode, headers);
-      response.end("Post Received")
+      response.end(JSON.stringify(dataObj));
     })
+  } else if ( request.method === 'OPTIONS') {
+        response.writeHead(statusCode, headers);
+        response.end(JSON.stringify(dataObj)); 
   }
 
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
-};
+
+
 
